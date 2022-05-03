5847d49fdd19fc69c454a1b5328b9cb7711213c7
catalamarti <marticatalasabate@gmail.com>
2022-05-03 20:08:41 +0100

    Delete Functions_1.R

--- Functions_1.R
+++
@@ -1,9 +0,0 @@
-# Lets filter people older than 80 (that were born before 1942)
-older80 <- filter(person_db,year_of_birth<=1942)
-older80
-# Lets select year of birth and person_id
-older80 <- select(filter(person_db,year_of_birth<=1942),person_id,year_of_birth)
-older80
-# Combine them in an easy code (%>%)
-older80 <- person_db %>% filter(year_of_birth<=1942) %>% select(person_id,year_of_birth)
-older80
